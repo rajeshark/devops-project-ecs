adc9d8fd1f996e54df592424730c317eee3680b7
rajeshark <101121907+rajeshark@users.noreply.github.com>
2025-10-23 19:22:20 +0530

    Update ecs-task-auth.tf

--- terraform/ecs-task-auth.tf
+++ terraform/ecs-task-auth.tf
@@ -9,46 +9,34 @@ resource "aws_ecs_task_definition" "auth" {
   task_role_arn      = "arn:aws:iam::775826428475:role/ecsTaskExecutionRole"
 
   container_definitions = jsonencode([
-    {
-      name       = "auth-service",
-      image      = "775826428475.dkr.ecr.ap-south-1.amazonaws.com/auth:latest",
-      essential  = true,
-      portMappings = [
-        {
-          containerPort = 5005,
-          hostPort      = 5005
-        }
-      ],
-      secrets = [
-        {
-          name      = "JWT_SECRET"
-          valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5"
-        },
-        {
-          name      = "DB_PORT"
-          valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5"
-        },
-        {
-          name      = "DB_NAME"
-          valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5"
-        },
-        {
-          name      = "DB_USER"
-          valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5"
-        },
-        {
-          name      = "DB_PASSWORD"
-          valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5"
-        },
-        {
-          name      = "DB_HOST"
-          valueFrom = aws_ssm_parameter.db_host.arn   # dynamic value from RDS endpoint
-        },
-        {
-          name      = "NODE_ENV"
-          valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5"
-        }
-      ]
+  {
+    name       = "auth-service"
+    image      = "775826428475.dkr.ecr.ap-south-1.amazonaws.com/auth:latest"
+    essential  = true
+    portMappings = [
+      {
+        containerPort = 5005
+        hostPort      = 5005
+      }
+    ]
+    secrets = [
+      { name = "JWT_SECRET", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5" },
+      { name = "DB_PORT", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5" },
+      { name = "DB_NAME", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5" },
+      { name = "DB_USER", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5" },
+      { name = "DB_PASSWORD", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5" },
+      { name = "DB_HOST", valueFrom = aws_ssm_parameter.db_host.arn },
+      { name = "NODE_ENV", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5" }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        "awslogs-group"         = "/ecs/${var.project_name}-auth"
+        "awslogs-region"        = "ap-south-1"
+        "awslogs-stream-prefix" = "ecs"
+        "awslogs-create-group"  = "true"
+      }
     }
-  ])
+  }
+])
 }
